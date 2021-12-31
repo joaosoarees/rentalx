9153512452f41990a75b7e3ead5958063657341e
João Vitor <jsoareez@gmail.com>
2021-12-31 01:50:31 -0300

    feat: hashPassword & non duplicated email

--- src/modules/accounts/useCases/createUser/createUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/createUserUseCase.ts
@@ -1,3 +1,4 @@
+import { hash } from 'bcryptjs';
 import { inject, injectable } from 'tsyringe';
 
 import { ICreateUserDTO } from '../../../dtos/ICreateUserDTO';
@@ -11,6 +12,16 @@ export class CreateUserUseCase {
   ) {}
 
   async execute(data: ICreateUserDTO): Promise<void> {
-    await this.usersRepository.create(data);
+    const userAlreadyExists = await this.usersRepository.findByEmail(
+      data.email
+    );
+
+    if (userAlreadyExists) {
+      throw new Error('User already exists');
+    }
+
+    const hashPassword = await hash(data.password, 8);
+
+    await this.usersRepository.create({ ...data, password: hashPassword });
   }
 }
