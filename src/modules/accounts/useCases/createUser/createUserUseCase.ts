912e62dc3353859acc49adcd476803341c03194f
João Vitor <jsoareez@gmail.com>
2021-12-31 01:08:31 -0300

    feat: create users repository & useCase

---
+++ src/modules/accounts/useCases/createUser/createUserUseCase.ts
@@ -0,0 +1,15 @@
+import { inject } from 'tsyringe';
+
+import { ICreateUserDTO } from '../../../dtos/ICreateUserDTO';
+import { IUsersRepository } from '../../repositories/IUsersRepository';
+
+export class CreateUserUseCase {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository
+  ) {}
+
+  async execute(data: ICreateUserDTO): Promise<void> {
+    await this.usersRepository.create(data);
+  }
+}
