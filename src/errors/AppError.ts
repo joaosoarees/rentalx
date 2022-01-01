c8d05d6e6a51a48e5a64e10d36e5ba39015e672e
João Vitor <jsoareez@gmail.com>
2022-01-01 16:38:42 -0300

    feat: error hanler

---
+++ src/errors/AppError.ts
@@ -0,0 +1,9 @@
+export class AppError {
+  private readonly message: string;
+  private readonly statusCode: number;
+
+  constructor(message: string, statusCode = 400) {
+    this.message = message;
+    this.statusCode = statusCode;
+  }
+}
